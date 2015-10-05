e38e122b558518bb834dcdd4a8d0fd094c61fd87
Kevin Payet <k_payet@yahoo.fr>
2015-10-05 16:02:15 +0200

    modifs to binary search

--- binarySearch/main.cpp
+++ binarySearch/main.cpp
@@ -4,8 +4,8 @@
 
 int main(void) {
     
-    std::vector<int> a{1,2,3,4,5,6,7,8,9};
-    std::cout<<search(a, 4)<<std::endl;
+    std::vector<int> v{1,2,3,4,5,6,7,8,9};
+    std::cout<<search(v, 4)<<std::endl;
     
     return 0;
 }
