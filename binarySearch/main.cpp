2c2d72ae351a8e0e5601cc43b71b7ed1d437ebec
Kevin Payet <k_payet@yahoo.fr>
2015-10-05 15:32:53 +0200

    main test

--- binarySearch/main.cpp
+++ binarySearch/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "binarySearch.h"
+#include <vector>
 
 int main(void) {
+    
+    vector<int> a = {1,2,3,4,5,6,7,8,9};
+    std::cout<<search(a, 4)<<endl;
     
     return 0;
 }
