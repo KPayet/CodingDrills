7197f86f4c9a0b68825ff9aa166ca80a9c20dad4
Kevin Payet <k_payet@yahoo.fr>
2015-10-05 16:22:58 +0200

    modifs to binary search

--- binarySearch/binarySearch.h
+++ binarySearch/binarySearch.h
@@ -2,8 +2,11 @@
 #define BINARY_SEARCH_H
 #include <vector>
 
+// Use of a template function so that it can be used with any type of data
+// PROVIDED it implements/overloads the comparison operators
+// Important note: Binary search is only meant for sorted arrays !!!
 template <typename T>
-int search(const std::vector<T> &a, T key){
+int search(const std::vector<T> &a, const T &key){
     int lo = 0;
     int hi = a.size()-1;
     
