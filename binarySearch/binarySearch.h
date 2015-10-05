aeb68125f35749e8d1b324be60efe57297dfd479
Kevin Payet <k_payet@yahoo.fr>
2015-10-05 15:27:36 +0200

    Added first version of binary search algorithm and empty main

---
+++ binarySearch/binarySearch.h
@@ -0,0 +1,8 @@
+#ifndef BINARY_SEARCH_H
+#define BINARY_SEARCH_H
+#include <vector>
+
+template <typename T>
+int search(const std::vector<T> &a, const T &key);
+
+#endif
---
+++ binarySearch/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "binarySearch.h"
+
+int main(void) {
+    
+    return 0;
+}
