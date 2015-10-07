8b49b56c90191be7e2fdd90efebb16e2843bc53a
Kevin Payet <k_payet@yahoo.fr>
2015-10-07 16:00:40 +0200

    working version of linkedList. Next add smart pointer + template. Iterator ?

--- linkedList/linkedList.h
+++ linkedList/linkedList.h
@@ -1,8 +1,10 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
+#include <iostream>
 
 class linkedList {
 private:
+
     // helper class Node
     class Node {
     public:
@@ -10,14 +12,94 @@ private:
         Node *prev;
         Node *next;
         Node(int val, Node* p, Node* n):data(val), prev(p), next(n) {}
+        ~Node(){}
     }; // end class Node
     Node *first;
     Node *last;
+    int N;
 public:
+
     linkedList() {
         first = nullptr;
         last = nullptr;
+        N=0;
+    }
+    ~linkedList() {
+        while(N)
+            removeFirst();
+        delete first;
+        delete last;
+    }
+    void addFirst(int data) {
+        first = new Node(data, nullptr, first);
+        if(!last)
+            last = first;
+        ++N;
+    }
+    void addLast(int data) {
+        if(!last) {
+            addFirst(data);
+            return;
+        }
+        last->next = new Node(data, last, nullptr);
+        last = last->next;
+        ++N;
+    }
+    void removeFirst() {
+        if(!first) return;
+        if(!first->next) {
+            delete first;
+            first = nullptr;
+            last = nullptr;
+            --N;
+            return;
+        }
+        Node *oldfirst = first;
+        first = first->next;
+        first->prev = nullptr;
+        delete oldfirst;
+        oldfirst = 0;
+        --N;
+    }
+    void removeLast() {
+        if(!last) return;
+        if(!last->prev){
+            delete last;
+            first = nullptr;
+            last = nullptr;
+            --N;
+            return;
+        }
+        Node *oldlast = last;
+        last = last->prev;
+        last->next = nullptr;
+        delete oldlast;
+        oldlast = 0;
+        --N;
+    }
+    int size() {
+        return N;
+    }
+    bool isEmpty() {
+        return N==0;
+    }
+
+    //overloading ostream operator to be able to use std::cout<<myLinkedList;
+    friend std::ostream& operator<< (std::ostream &out, const linkedList &lList){
+        if(!lList.N) {
+            out<<" ";
+            return out;
+        }
+        Node *x = lList.first;
+        while(x) {
+            out<<x->data<<" ";
+            x = x->next;
+        }
+        return out;
+    }
+    friend std::ostream& operator<< (std::ostream &out, const linkedList *lList){
+        out<<*lList;
+        return out;
     }
 };
-
 #endif // LINKEDLIST_H
--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -3,7 +3,25 @@
 
 int main(int argc, char *argv[]){
 
-    linkedList *list = new linkedList();
+    linkedList *l = new linkedList();
+    std::cout<<l<<std::endl;
+    l->addFirst(6);
+    std::cout<<l<<std::endl;
+    l->addLast(12);
+    std::cout<<l<<std::endl;
+    l->addFirst(6);
+    std::cout<<l<<std::endl;
+    l->addFirst(6);
+    std::cout<<l<<std::endl;
+    l->addLast(18);
+    std::cout<<l<<std::endl;
+    l->removeLast();
+    std::cout<<l<<std::endl;
+    l->removeLast();
+    std::cout<<l<<std::endl;
+    l->removeFirst();
+
+    delete l;
 
     return 0;
 }
