9b65fccb7dc5058147c3d6d1d5ee9c076fedd8f0
Kevin Payet <k_payet@yahoo.fr>
2015-10-19 15:40:11 +0200

    first (almost) working version of Queue. Need to check for dequeue from empty Queue and it should be good

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -2,6 +2,7 @@
 #define QUEUE_H
 
 #include "LinkedList.h"
+#include <cassert>
 
 ///
 /// Simple Queue class, not double-ended
@@ -13,18 +14,20 @@ class Queue
 private:
 
     ///
-    /// Internal linked list the queue is based upon
+    /// Internal linked list the queue is based upon.
+    /// As you will see below, it is this class that does all the work
+    /// The Queue class is simply just ensuring that the list is used FIFO-style
     /// For more details about that class, see ../LinkedList/LinkedList.h
     ///
-    LinkedList<int> intList;
+    LinkedList<int> intList; /// the list is dynamic.
 
-    public:
+public:
 
         ///
         /// Constructors
         ///
-        Queue();
-        virtual ~Queue();
+        Queue() {}
+        virtual ~Queue() {}
 
         ///
         /// enqueue and dequeue operations
@@ -35,12 +38,39 @@ private:
         void enqueue(int v) {
             intList.addLast(v);
         }
+
         int dequeue() {
-            int item = l.first->data;
-            l.removeFirst();
+            assert(!this->isEmpty() && "Trying to dequeue from empty Queue!");
+            int item = intList.getFront();
+            intList.removeFirst();
+
+            return item;
         }
 
-    private:
+        /// Empty Queue
+        /// Simply use the clear routine in list implementation
+        void clear() {
+            intList.clear();
+        }
+
+        ///
+        /// Members that don't modify the Queue
+        /// Could be const functions, but I don't really see the point of declaring a const Queue
+        ///
+
+        /// If you want to see the next item to be dequeued
+        /// but don't want it removed
+        int peek() {
+            return intList.getFront();
+        }
+
+        int size() { /// self-explanatory
+            return intList.size();
+        }
+
+        bool isEmpty() {
+            return intList.isEmpty();
+        }
 };
 
 #endif // QUEUE_H
--- Queue/main.cpp
+++ Queue/main.cpp
@@ -1,10 +1,24 @@
 #include "Queue.h"
 
 #include <iostream>
+#include <vector>
 
 int main(int argc, char* argv[]) {
 
+    using std::cout;
+    using std::endl;
 
+    Queue q;
+
+    for(int i=1; i<20; i++)
+        q.enqueue(i);
+    cout<<q.size()<<endl;
+    cout<<q.peek()<<endl;
+
+    while(!q.isEmpty())
+        cout<<q.dequeue()<<endl;
+
+    q.dequeue();
 
     return 0;
 }
