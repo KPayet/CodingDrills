1d57d01bae13bf5c15ec4cf8261c0af6a7965367
Kevin Payet <k_payet@yahoo.fr>
2015-11-03 13:54:19 +0100

    added union and find implementation.

--- Graphs/UnionFind/UF.cpp
+++ Graphs/UnionFind/UF.cpp
@@ -1,7 +1,6 @@
 #include "UF.h"
 
-UF::UF(int N)
-{
+UF::UF(int N) {
     nComponents = N;
     parent = new int[N];
     sz = new int[N];
@@ -11,8 +10,46 @@ UF::UF(int N)
     }
 }
 
-UF::~UF()
-{
+UF::~UF() {
     delete[] parent;
     delete[] sz;
+}
+
+///
+/// Like explained in UF.h, union sets the root of smaller subtree to the bigger one
+///
+void UF::union(int p, int q) {
+
+    int pRoot = find(p);
+    int qRoot = find(q);
+
+    /// if the sites are already connected, do nothing
+    if(pRoot == qRoot) return;
+
+    /// Again, we simply connect the root of the smaller subtree to the root of the bigger one
+    if(sz[pRoot] < sz[qRoot]) { parent[pRoot] = qRoot; sz[qRoot] += sz[pRoot]; }
+    else { parent[qRoot] = pRoot; sz[pRoot] += sz[qRoot]; }
+
+    count--; /// we just connected two components, so we have one less
+}
+
+int UF::find(int p) {
+
+    int root = p;
+
+    while(root != parent[root]) /// this here is the find routine. A site is represented by the root of its subtree
+        root = parent[root];
+
+    /// This second part is called path compression
+    /// It is meant solely as an optimization step
+    /// It allows to get trees that are even shallower
+    /// In terms of performance, Weighted Quick Union guarantees O(logN) time for union and find
+    /// With path compression, we get ~O(1) time, i.e. nearly constant (even if not really)
+    while(p != root) {
+        int newp = p;
+        parent[p] = root;
+        p = newp;
+    }
+
+    return root;
 }
--- Graphs/UnionFind/UF.h
+++ Graphs/UnionFind/UF.h
@@ -21,12 +21,35 @@
 // Author: Kevin Payet
 // ---------------------------------------------------------------------------
 
+///
+/// The weighted Quick Union implementation uses a tree representation for our data
+/// Each site has a parent site, except for the root of a subtree which is its own parent
+/// In this representation, a connected component corresponds to one subtree
+/// So, sites are connected if they belong to the same subtree
+/// The "trick" of UF data structure is that we only care about overall connectivity
+/// not about the paths that connect two sites.
+/// So, we don't need a traditional tree implementation, with Nodes etc.
+/// All we require is an array (i.e. parent array below), to maintain the connectivity information
+///
 class UF
 {
 private:
         int nComponents;  // number of connected components
+        ///
+        /// The tree structure is entirely defined by the parent array
+        ///
         int *parent; // parent[i] gives the parent of site i
+        ///
+        /// The sz array is why this implementation is called Weighted
+        /// In the simple Quick Union implementation, when doing union between two sites,
+        /// we set the root of the subtree of one of the sites to be equal to the root of the other subtree
+        /// i.e. we randomly connect one of the subtrees to the other one
+        /// But, this can cause efficiency problems, because trees get tall (large depth)
+        /// Weighting solves this problem by connecting the SMALLER tree to the bigger one.
+        /// And the information about the sizes of subtrees is stored inside this sz array...
+        ///
         int *sz;    // sz[i] gives the size of the subtree site i belongs to
+
 public:
         ///
         /// Initializes a Union-Find data structure with N sites
@@ -35,8 +58,8 @@ public:
 
         ~UF();
 
-//        void union(int p, int q);
-//        int find(int p);
+        void union(int p, int q);
+        int find(int p);
 //        bool connected(int p, int q);
 //        int count() {return nComponents;}
 };
