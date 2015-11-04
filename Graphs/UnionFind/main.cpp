30d60bd3b8f545b211aa4fc335b1ea66b9a5b904
Ubuntu <ubuntu@ip-172-31-36-82.eu-west-1.compute.internal>
2015-11-04 14:07:27 +0000

    corrected typo in main .cpp

--- Graphs/UnionFind/main.cpp
+++ Graphs/UnionFind/main.cpp
@@ -2,7 +2,6 @@
 
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <string>
 
 int main(int argc, char *argv[])
@@ -21,11 +20,10 @@ int main(int argc, char *argv[])
     int counter = 0;
 
     while(std::getline(input, line) && counter<1000) {
-        cout<<line<<endl;
         int p = std::stoi(line.substr(0, line.find(" ")));
         int q = std::stoi(line.substr(line.find(" ") + 1, line.length() - line.find(" ")));
-        cout<<p<<" "<<q<<endl;
-        //uf->Union()
+        
+	uf->Union(p, q);
         ++counter;
     }
 
