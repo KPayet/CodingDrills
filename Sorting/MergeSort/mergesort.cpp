fd33f3916eec78853e7e3cfa49784c29cbb5d876
Kevin Payet <k_payet@yahoo.fr>
2015-11-06 14:04:36 +0100

    added helper function to mergesort

--- Sorting/MergeSort/mergesort.cpp
+++ Sorting/MergeSort/mergesort.cpp
@@ -2,13 +2,13 @@
 
 namespace Merge{
 
+static void sort(std::vector<int> &a, std::vector<int> &aux, int lo, int hi){
+
+}
+
 void sort(std::vector<int> &a) {
 
     std::vector<int> aux;
     sort(a, aux, 0, a.size()-1);
-}
-
-static void sort(std::vector<int> &a, std::vector<int> &aux, int lo, int hi){
-
 }
 }
