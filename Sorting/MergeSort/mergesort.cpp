1534bd7ba62075e66ccb494dcc5de6125367ce06
Kevin Payet <k_payet@yahoo.fr>
2015-11-06 14:14:40 +0100

    few modifs

--- Sorting/MergeSort/mergesort.cpp
+++ Sorting/MergeSort/mergesort.cpp
@@ -7,8 +7,7 @@ static void sort(std::vector<int> &a, std::vector<int> &aux, int lo, int hi){
 }
 
 void sort(std::vector<int> &a) {
-
-    std::vector<int> aux;
-    sort(a, aux, 0, a.size()-1);
+    std::vector<int> aux;   // mergesort is not in place and requires an auxiliary array
+    sort(a, aux, 0, a.size()-1);    // does all the work, but stays in the shadows...
 }
 }
