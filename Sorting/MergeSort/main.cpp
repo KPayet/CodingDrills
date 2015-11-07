b5da9bb6c73121f65911a39b1fe22b53e59717fe
Kevin Payet <k_payet@yahoo.fr>
2015-11-07 11:46:12 +0100

    added test to main.cpp

--- Sorting/MergeSort/main.cpp
+++ Sorting/MergeSort/main.cpp
@@ -1,11 +1,15 @@
 #include "mergesort.h"
 #include <vector>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     std::vector<int> v {9,7,3,6,8,4,9,1,13,12,1,0,9,7};
 
     Merge::sort(v);
+
+    for(auto x: v)
+        std::cout<<x<<std::endl;
 
     return 0;
 }
