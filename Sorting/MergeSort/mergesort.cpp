9573ce95ec5786e8664dd593832c2bf0ba351830
Kevin Payet <k_payet@yahoo.fr>
2015-11-07 11:07:48 +0100

    modified mergesort.cpp

--- Sorting/MergeSort/mergesort.cpp
+++ Sorting/MergeSort/mergesort.cpp
@@ -1,15 +1,16 @@
 #include "mergesort.h"
 
+namespace Merge{
+
 namespace {
 void sort(std::vector<int> &a, std::vector<int> &aux, int lo, int hi){
 
 }
 }
 
-namespace Merge{
-
 void sort(std::vector<int> &a) {
     std::vector<int> aux;   // mergesort is not in place and requires an auxiliary array
     sort(a, aux, 0, a.size()-1);    // does all the work, but stays in the shadows...
 }
+
 }
