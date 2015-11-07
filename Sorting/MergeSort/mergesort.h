2992a229997da821f4667e66a082c8e9086a5f37
Kevin Payet <k_payet@yahoo.fr>
2015-11-07 17:27:31 +0100

    reverse call to input and auxilliary arrays in sorting function. This allows to spare the initial copy in merge function

--- Sorting/MergeSort/mergesort.h
+++ Sorting/MergeSort/mergesort.h
@@ -28,21 +28,21 @@ void merge(std::vector<T> &a, std::vector<T> &aux, int lo, int mid, int hi){
     // We merge a[lo:mid] with a[mid+1:hi]
     int i = lo, j = mid+1;
 
-    for (int k = lo; k <= hi; k++)
-        aux[k] = a[k];
+//    for (int k = lo; k <= hi; k++)
+//        aux[k] = a[k];
 
     /// now we copy-back to a, but in sorted order
     /// Merging is simply comparing each element of the two sub-arrays with indexes i and j defined above
     /// and copy the smallest one to the original array, while incrementing the corresponding index.
     /// We simply need to be careful with the sub-arrays limit, i.e. watch out for when i>mid, or j>hi
-    for (int k = lo; k <= hi; k++){
+    for (int k = lo; k <= hi; ++k){
         // taking care of limit cases
         // basically, if we have already reached the limit of one sub-array,
         // we simply copy the rest of the other one to a.
-        if (i > mid) a[k] = aux[j++];
-        else if (j > hi ) a[k] = aux[i++];
-        else if (aux[j] < aux[i]) a[k] = aux[j++];
-        else a[k] = aux[i++];
+        if (i > mid) aux[k] = a[j++];
+        else if (j > hi ) aux[k] = a[i++];
+        else if (a[j] < a[i]) aux[k] = a[j++];
+        else aux[k] = a[i++];
     }
 }
 
@@ -63,7 +63,7 @@ template <typename T>
 void sort(std::vector<T> &a, std::vector<T> &aux, int lo, int hi){
 
     if(hi <= lo + cutoffToInsertionSort){ // for small arrays, we cut to insertion sort
-        insertionSort(a, lo, hi);
+        insertionSort(aux, lo, hi);
         return;
     }
 
@@ -73,8 +73,8 @@ void sort(std::vector<T> &a, std::vector<T> &aux, int lo, int hi){
 
     /// Recursive sorting of each part
     /// i.e. Divide...
-    sort(a, aux, lo, mid);
-    sort(a, aux, mid+1, hi);
+    sort(aux, a, lo, mid);
+    sort(aux, a, mid+1, hi);
 
     /// ...and Conquer
     /// We merge the two sorted sub-arrays back together
@@ -85,7 +85,7 @@ void sort(std::vector<T> &a, std::vector<T> &aux, int lo, int hi){
 template <typename T>
 void sort(std::vector<T> &a) {
     std::vector<T> aux(a.size(), 0);   // mergesort is not in place and requires an auxiliary array
-    sort(a, aux, 0, a.size()-1);    // does all the work, but stays in the shadows...
+    sort(aux, a, 0, a.size()-1);    // does all the work, but stays in the shadows...
 }
 
 } // namespace Merge
