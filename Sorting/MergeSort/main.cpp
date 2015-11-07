749de461471ce3bcd7dd28d0ed5b595fab52dc36
Kevin Payet <k_payet@yahoo.fr>
2015-11-07 12:03:23 +0100

    start of time testing

--- Sorting/MergeSort/main.cpp
+++ Sorting/MergeSort/main.cpp
@@ -1,15 +1,24 @@
 #include "mergesort.h"
 #include <vector>
 #include <iostream>
+#include <cassert>
+#include <random>
+#include <string>
 
 int main(int argc, char *argv[])
 {
-    std::vector<int> v {9,7,3,6,8,4,9,1,13,12,1,0,9,7};
+//    std::vector<int> v {9,7,3,6,8,4,9,1,13,12,1,0,9,7};
+//
+//    Merge::sort(v);
+//
+//    for(auto x: v)
+//        std::cout<<x<<std::endl;
 
-    Merge::sort(v);
+    int N = 1000;
+    if(argc < 2)
+        std::cout<<"Usage: ./main 500 \n Defaulting to 1000"<<std::endl;
+    else N = std::stoi(string(argv[1]));
 
-    for(auto x: v)
-        std::cout<<x<<std::endl;
 
     return 0;
 }
