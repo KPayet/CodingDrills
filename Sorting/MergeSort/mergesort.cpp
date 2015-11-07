5c45d589bdda162fa5ffd8b04f89f53976862cb2
Kevin Payet <k_payet@yahoo.fr>
2015-11-07 11:44:25 +0100

    needed to initialize aux to all 0s

--- Sorting/MergeSort/mergesort.cpp
+++ Sorting/MergeSort/mergesort.cpp
@@ -47,7 +47,7 @@ void sort(std::vector<int> &a, std::vector<int> &aux, int lo, int hi){
 }
 
 void sort(std::vector<int> &a) {
-    std::vector<int> aux;   // mergesort is not in place and requires an auxiliary array
+    std::vector<int> aux(a.size(), 0);   // mergesort is not in place and requires an auxiliary array
     sort(a, aux, 0, a.size()-1);    // does all the work, but stays in the shadows...
 }
 
