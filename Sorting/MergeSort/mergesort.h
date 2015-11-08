fe6a70e01228afdc240f857557c8aaac8be333bd
Kevin Payet <k_payet@yahoo.fr>
2015-11-08 11:19:01 +0100

    cleared some comments

--- Sorting/MergeSort/mergesort.h
+++ Sorting/MergeSort/mergesort.h
@@ -9,7 +9,7 @@
 // UF.H
 // This header simply implements a template sorting function, using mergesort algorithm
 // It can be used to sort any type that implements comparison operators
-// The algorithm implements a cutoff to insertion sort for arrays smaller than 10 items
+// The algorithm implements a cutoff to insertion sort for arrays smaller than 7 items
 //
 // Author: Kevin Payet
 // ---------------------------------------------------------------------------
@@ -23,7 +23,7 @@ namespace {
 const int cutoffToInsertionSort = 7;
 
 template <typename T>
-void merge(std::vector<T> &a, std::vector<T> &aux, int lo, int mid, int hi){
+void merge(std::vector<T> &aux, std::vector<T> &a, int lo, int mid, int hi){
 
     // We merge a[lo:mid] with a[mid+1:hi]
     int i = lo, j = mid+1;
@@ -39,10 +39,10 @@ void merge(std::vector<T> &a, std::vector<T> &aux, int lo, int mid, int hi){
         // taking care of limit cases
         // basically, if we have already reached the limit of one sub-array,
         // we simply copy the rest of the other one to a.
-        if (i > mid) aux[k] = a[j++];
-        else if (j > hi ) aux[k] = a[i++];
-        else if (a[j] < a[i]) aux[k] = a[j++];
-        else aux[k] = a[i++];
+        if (i > mid) a[k] = aux[j++];
+        else if (j > hi ) a[k] = aux[i++];
+        else if (aux[j] < aux[i]) a[k] = aux[j++];
+        else a[k] = aux[i++];
     }
 }
 
@@ -60,10 +60,10 @@ void insertionSort(std::vector<T> &a, int lo, int hi){
 }
 
 template <typename T>
-void sort(std::vector<T> &a, std::vector<T> &aux, int lo, int hi){
+void sort(std::vector<T> &aux, std::vector<T> &a, int lo, int hi){
 
     if(hi <= lo + cutoffToInsertionSort){ // for small arrays, we cut to insertion sort
-        insertionSort(aux, lo, hi);
+        insertionSort(a, lo, hi);
         return;
     }
 
@@ -73,12 +73,12 @@ void sort(std::vector<T> &a, std::vector<T> &aux, int lo, int hi){
 
     /// Recursive sorting of each part
     /// i.e. Divide...
-    sort(aux, a, lo, mid);
-    sort(aux, a, mid+1, hi);
+    sort(a, aux, lo, mid);
+    sort(a, aux, mid+1, hi);
 
     /// ...and Conquer
     /// We merge the two sorted sub-arrays back together
-    merge(a, aux, lo, mid, hi);
+    merge(aux, a, lo, mid, hi);
 }
 } // namespace
 
