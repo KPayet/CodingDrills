bce45e1a21efb60903567e7ed23a131c43a880de
Kevin Payet <k_payet@yahoo.fr>
2015-11-11 12:03:21 +0100

    added directory for priority queue class + initial files

---
+++ PriorityQueue/PQ.h
@@ -0,0 +1,18 @@
+#ifndef PQ_H
+#define PQ_H
+
+// ---------------------------------------------------------------------------
+// This software is in the public domain, furnished "as is", without technical
+// support, and with no warranty, express or implied, as to its usefulness for
+// any purpose.
+//
+// PQ.H
+// This file is a template class implementation for the priority queue data structure
+// It offers both MaxPQ and MinPQ which are just aliases defined from the main class implementation
+//
+// Author: Kevin Payet
+// ---------------------------------------------------------------------------
+
+
+
+#endif // PQ_H
