58988289d410e43c7668eba620e7238656cb1e0f
Kevin Payet <k_payet@yahoo.fr>
2015-11-11 10:31:12 +0100

    better comments in main.cpp + completed README - Final version of algorithm (is it?)

--- Sorting/QuickSort/README.md
+++ Sorting/QuickSort/README.md
@@ -4,8 +4,13 @@ A pretty efficient implementation of quicksort. Can be used for any type that im
 The present implementation requires C++11.
 
 This implementation of quicksort uses Bentley-McIlroy 3-way partitioning which gives pretty good results. You can see here: http://www.sorting-algorithms.com/static/QuicksortIsOptimal.pdf, pages 8 and 9 for the model used for this implementation. It is really worth it in terms of performance, compared to standard Dijsktra 3-way partitioning.
+
 There is also a cutoff to insertion sort for arrays smaller than 7 elements.
 Finally, the pivot element for partitioning is computed using Tukey's ninther (= median of 3 medianS of 3), or simple median of 3 for smaller arrays.
+
 The biggest factor for the performance seems to be the partitioning algorithm used, which is what we would expect...
 
 
+I have implemented the shuffling step, that is usually performed as the very first step of the quicksort algorithm, as an option. That's because, for some application, one might judge that this step is useless, and that it's safe to use without shuffling.
+
+In that case, we achieve better performance than C++11's std::sort. With shuffling, we get pretty much the same performance.
--- Sorting/QuickSort/main.cpp
+++ Sorting/QuickSort/main.cpp
@@ -16,19 +16,25 @@ int main(int argc, char *argv[])
 //    for(auto x: v)
 //        std::cout<<x<<std::endl;
 
-    int N = 1000000;
+    int N = 1000;
     if(argc < 2)
         std::cout<<"Usage: ./main 500 \n Defaulting to 1000"<<std::endl;
-    //else N = std::stoi(std::string(argv[1]));
+    else N = std::stoi(std::string(argv[1]));
 
+    ///
+    /// Generate random input vector
+    ///
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0, 10000000);
 
     std::vector<double> v;
-
     for(int i=0; i<N; ++i)
         v.push_back(dis(gen));
+
+    ///
+    /// Sort with std::sort, record time
+    ///
 
     std::vector<double> v1 = v; // so that I use the exact same inputs
 
@@ -40,7 +46,10 @@ int main(int argc, char *argv[])
     if(!std::is_sorted(v1.begin(), v1.end()))
         std::cout<<"Not Sorted!!!!"<<std::endl;
 
-    /// Now, my implementation
+    ///
+    /// Now, with Quick::sort
+    ///
+
     std::vector<double> v2 = v; // so that I use the exact same inputs
 
     startTime = GetTimeMs64();
@@ -48,9 +57,10 @@ int main(int argc, char *argv[])
     endTime = GetTimeMs64();
     std::cout<<"\nQuick::sort: "<<N<<" "<<(endTime - startTime)<<std::endl;
 
-    if(!std::is_sorted(v2.begin(), v2.end()))
+    if(!std::is_sorted(v2.begin(), v2.end())) // check if sorted
         std::cout<<"Not Sorted!"<<std::endl;
 
+    /// check that both outputs are equal
     if( v1 == v2 )
 	std::cout<<"\n\nSame result!"<<std::endl;
 
