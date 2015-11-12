ce4eea0862bfdf9c6359aca95af061d8273ea309
Kevin Payet <k_payet@yahoo.fr>
2015-11-12 10:49:13 +0100

    add enqueue, dequeue, swim. needs sink and some cleaning

--- PriorityQueue/PQ.h
+++ PriorityQueue/PQ.h
@@ -13,28 +13,70 @@
 // Author: Kevin Payet
 // ---------------------------------------------------------------------------
 
+#include <vector>
+
 class PQ {
+
+private:
+    int N;  // queue size
+    std::vector<int> pq;
+
+    ///helper functions
+
+    void exch(const int i, const int j); // swaps items pq[i] and pq[j]
+    // this function is in charge of the comparison (i.e. < or > ?)
+    bool heapCompare(int a, int b){ // true if
+
+    }
+
+    /// helper functions used to implement insert and get
+    /// swim and sink move the item respectively up and down the heap,
+    /// until the heap property is satisfied.
+    void swim(int i){
+        // while heap property is not satisfied between i and its parent,
+        // swap both elements
+        while(i > 1 && pq[i] > pq[i/2]){
+            exch(i, i/2);
+            i = i/2;
+        }
+    }
+    void sink(int i);
 
 public:
 
     /// Constructors + Destructor
-    PQ() {}
-    PQ(int maxN) {}
+    PQ(): N(0) {
+        pq[0] = nullptr;
+    }
+    PQ(int maxN):PQ(){
+        pq.reserve(maxN);
+        pq[0] = nullptr;
+    }
 
     ~PQ() {}
 
     /// Functions that access data/ modify data structure
 
-    void enqueue(int data);
+    void enqueue(int data){
+        pq[++N] = data;
+        swim(N);
+    }
     /// returns and remove min/max element from the queue
-    int dequeue();
-    int peek(); // just returns min/max element without
+    int dequeue(){
+        int item = pq[1]; // min or max depending on the type of queue
+        exch(1, N--);
+        sink(1);
+        pq.erase(N+1);
+        return item;
+    }
+    int peek(){ // just returns min/max element without
+        return pq[1];
+    }
 
     /// diagnostics
 
-    int size();
-    bool isEmpty();
-
+    int size(){ return N;}
+    bool isEmpty(){ return N == 0;}
 };
 
 #endif // PQ_H
--- PriorityQueue/main.cpp
+++ PriorityQueue/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "PQ.h"
 
 int main(int argc, char *argv[])
 {
