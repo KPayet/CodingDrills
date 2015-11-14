fb322c65cdc4a0149289890758a57d0b7801f711
Kevin Payet <k_payet@yahoo.fr>
2015-11-14 12:31:57 +0100

    Complete working template PQ class. Need to test performances a bit...

--- PriorityQueue/PQ.h
+++ PriorityQueue/PQ.h
@@ -20,9 +20,16 @@
 #include <vector>
 #include <iostream>
 
+///
+/// Declaration of generic template priority queue class
+/// isMax boolean controls if we want a MinPQ (priority to smaller elements) or MaxPQ
+/// Note that two aliases are defined at the bottom of this file: MinPQ and MaxPQ, that should be used by the user
+///
+template <class T, bool isMax> // isMax == true => we need MaxPQ
 class PQ {
 
-public:
+/// I declare protected constructors, because I want the class to be accessed through the MinPQ and MaxPQ specialized classes only (see bottom)
+protected:
 
     /// Constructors + Destructor
     PQ(): N(0) {}
@@ -34,33 +41,25 @@ public:
 
     ~PQ() {}
 
+public:
+
     /// Functions that access data/ modify data structure
 
-    void enqueue(int data){
+    void enqueue(T data){
         pq.push_back(data);
         swim(N++);
     }
     /// returns and remove min/max element from the queue
-    int dequeue(){
-        int item = pq[0]; // min or max depending on the type of queue
+    T dequeue(){
+        T item = pq[0]; // min or max depending on the type of queue
         exch(0, --N);
         sink(0);
         pq.erase(pq.begin() + N);
         return item;
     }
 
-    int peek(){ // just returns min/max element without
+    T peek(){ // just returns min/max element without
         return pq[0];
-    }
-
-    /// for testing
-
-    void printAll(){
-        std::cout<<"Size = "<<size()<<std::endl;
-        for(auto x: pq) {
-            std::cout<<x;
-        }
-        std::cout<<std::endl;
     }
 
     /// diagnostics
@@ -70,18 +69,21 @@ public:
 
 private:
     int N;  // queue size
-    std::vector<int> pq;
+    std::vector<T> pq; // actually holds the elements
 
     ///helper functions
 
     void exch(const int i, const int j){ // swaps items pq[i] and pq[j]
-        int tmp = pq[i];
+        T tmp = pq[i];
         pq[i] = pq[j];
         pq[j] = tmp;
     }
     // this function is in charge of the comparison (i.e. < or > ?)
-    bool heapCompare(int a, int b){ // true if
+    // this is where we tell that we want a Min or MaxPQ
+    // ALl it takes is inverting the direction of the comparison operator
+    bool heapOrdered(int a, int b){
 
+        return isMax ? pq[a] < pq[b] : pq[a] > pq[b];
     }
 
     /// helper functions used to implement insert and get
@@ -91,7 +93,7 @@ private:
         // while heap property is not satisfied between i and its parent,
         // swap both elements
         int p = i%2 == 0 ? i/2-1 : i/2;
-        while(i > 0 && pq[i] > pq[p]){
+        while(i > 0 && !heapOrdered(i, p)){
             exch(i, p);
             i = p ;
             p = i%2 == 0 ? i/2-1 : i/2;
@@ -99,15 +101,35 @@ private:
     }
 
     void sink(int i){
+        /// repeatedly move element down the heap until heap property is once again satisfied
         while (2*i+1 <= N-1)
         {
             int j = 2*i+1; // in a binary heap, children of i are in indexes 2*i+1 and 2*i+2
-            if (j < N-1 && pq[j] < pq[j+1]) j++; // select largest of two children
-            if (pq[i] > pq[j]) break;   // if pq[i] is larger than the largest child, then it's all good
+            if (j < N-1 && !heapOrdered(j+1, j)) j++; // select largest of two children
+            if (heapOrdered(j, i)) break;   // if pq[i] is larger than the largest child, then it's all good
             exch(i, j);
             i = j;
         }
     }
+};
+
+/// Aliases to be used by the user to create a Min or MaxPQ
+/// First I simply created aliases with using MinPQ = PQ<T, false>; ...
+/// But I couldn't find a way to limit access to PQ itself
+/// By creating two new classes, that simply specializes PQ (for isMax = true/false),
+/// I can hide PQ, and have users call only MinPQ and MaxPQ - Is there a simpler way?
+template <class T>
+class MaxPQ: public PQ<T, true> {
+public:
+    MaxPQ() {}
+    MaxPQ(int maxN): PQ<T, true>(maxN) {}
+};
+
+template <class T>
+class MinPQ: public PQ<T, false> {
+public:
+    MinPQ() {}
+    MinPQ(int maxN): PQ<T, false>(maxN) {}
 };
 
 #endif // PQ_H
--- PriorityQueue/main.cpp
+++ PriorityQueue/main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include "PQ.h"
 #include <vector>
+#include <random>
+//#include "benchmark.h"
+#include <algorithm>
 
 int main(int argc, char *argv[])
 {
     using std::cout;
     using std::endl;
 
-    PQ *pq = new PQ(10);
+    MinPQ<double> *pq = new MinPQ<double>(10);
 
     //cout<<pq->size()<<endl;
 
-    std::vector<int> v{1,2,3,4,5,6,12,54,21,1,1,56,8,10,10,8,100,6};
+    /// Generate random input vector
+    const int N = 10000;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0, 1000);
+
+    std::vector<double> v;
+    for(int i=0; i<N; ++i)
+        v.push_back(dis(gen));
+
 
     for(auto x: v) {
         pq->enqueue(x);
