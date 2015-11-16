c845cf8374ba9fd6d23409d921f411c82ac3de20
Kevin Payet <k_payet@yahoo.fr>
2015-11-16 10:36:33 +0100

    final version. Added README + testing in main.cpp

--- PriorityQueue/README.md
+++ PriorityQueue/README.md
@@ -1,1 +1,7 @@
 # Priority Queue
+
+Binary heap implementation of the Priority Queue data structure.
+PQ.h contains a template class implementation for MinPQ and MaxPQ, i.e. priority queues where respectively the minimum and the maximum element has highest priority.
+
+This can be used with any type that implements comparison operators.
+
--- PriorityQueue/main.cpp
+++ PriorityQueue/main.cpp
@@ -2,7 +2,7 @@
 #include "PQ.h"
 #include <vector>
 #include <random>
-//#include "benchmark.h"
+#include "benchmark.h"
 #include <algorithm>
 
 int main(int argc, char *argv[])
@@ -10,29 +10,45 @@ int main(int argc, char *argv[])
     using std::cout;
     using std::endl;
 
-    MinPQ<double> *pq = new MinPQ<double>(10);
-
-    //cout<<pq->size()<<endl;
+    MinPQ<double> *pq = new MinPQ<double>();
 
     /// Generate random input vector
-    const int N = 10000;
+    const int N = 4000000;
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(0, 1000);
+    std::uniform_real_distribution<> dis(0, 100000);
 
     std::vector<double> v;
     for(int i=0; i<N; ++i)
         v.push_back(dis(gen));
 
-
     for(auto x: v) {
         pq->enqueue(x);
     }
 
-    while(!pq->isEmpty()) {
-        cout<<pq->dequeue()<<endl;
+    ///
+    /// And here we can start the test, by enqueuing each time the same number in a queue of variable size
+    ///
+
+    const int N2 = 200000;
+    std::vector<double> v2;
+    for(int i=0; i<N2; ++i)
+        v2.push_back(dis(gen));
+
+    uint64_t starttime = GetTimeMs64();
+    for(auto x: v2) {
+        pq->enqueue(x);
     }
+    uint64_t endtime = GetTimeMs64();
+    cout<<"average enqueue time "<<N<<" "<<static_cast<double>(endtime-starttime)/N2<<endl;
+
+    starttime = GetTimeMs64();
+    for(int i=0; i<N2; i++) {
+        pq->dequeue();
+    }
+    endtime = GetTimeMs64();
+    cout<<"average dequeue time "<<N<<" "<<static_cast<double>(endtime-starttime)/N2<<endl;
 
     return 0;
 }
