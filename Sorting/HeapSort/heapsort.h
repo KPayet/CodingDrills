fa443452b43d30e974882ca60fd9b1d1c0660a76
Kevin Payet <k_payet@yahoo.fr>
2015-11-19 11:09:40 +0100

    Changed some comments

--- Sorting/HeapSort/heapsort.h
+++ Sorting/HeapSort/heapsort.h
@@ -15,13 +15,17 @@
 // ---------------------------------------------------------------------------
 
 // This algorithm reuses the components of priority queue implementation
-//  namely, the sink and sink and exch function.
+//  namely, the sink and exch functions.
 //  HeapSorting is simply reordering an array so that it satisfies heap property,
 //  and then perform the same operations we use when dequeuing, without actually removing the element
 //
-//  Merge end Quick sorts are most commonly used over HeapSort
+//  Merge and Quick sorts are most commonly used over HeapSort
 //  However, due to the small code required for its implementation, it can be used when
 //  code size matters, e.g. embedded system
+//
+//  Usage:
+//  std::vector<int> a { ... };
+//  Heap::sort(a);
 
 #include <vector>
 
@@ -40,8 +44,8 @@ template <typename T>
 void sink(std::vector<T> &a, int i, int N) {
     ///
     /// In HeapSort, the binary heap is a Max heap, meaning that the children are smaller than their parent.
-    /// To sort, we simply to the same operation as dequeue:
-    ///     i - swap first element (largest) with the last one
+    /// To sort, we simply perform the same operations as dequeue:
+    ///     i - swap first element in array a[0] (largest) with the last one a[N-1]
     ///    ii - sink the new first element
     /// This way, by iteratively putting the max element last in the array, we end up with a sorted array.
     ///
@@ -58,9 +62,15 @@ void sink(std::vector<T> &a, int i, int N) {
 
 template <typename T>
 void sort(std::vector<T> &a) {
+    
     int N = a.size();
+    
+    // Rearrange array so that it satisfies the heap property for a MAX heap
     for (int k = N/2; k >= 0; k--)
         sink(a, k, N);
+    
+    // And now, we simply swap repeatedly the first element with the last one, 
+    // and restore the heap property each time
     while (N > 1) {
         exch(a, 0, --N);
         sink(a, 0, N);
