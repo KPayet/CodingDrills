ea420f8da2f6a8851d8dc557f0b5354f5e5b0b56
Kevin Payet <k_payet@yahoo.fr>
2015-11-19 11:20:58 +0100

    first commit for binary search tree

---
+++ BinarySearchTree/BST.h
@@ -0,0 +1,15 @@
+#ifndef BST_H
+#define BST_H
+
+// ---------------------------------------------------------------------------
+// This software is in the public domain, furnished "as is", without technical
+// support, and with no warranty, express or implied, as to its usefulness for
+// any purpose.
+//
+// BST.H
+//
+// Author: Kevin Payet
+// ---------------------------------------------------------------------------
+
+
+#endif // BST_H
