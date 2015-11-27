ddf13c3e3ce9cb79b2ae56bc38115dd77a637482
Kevin Payet <k_payet@yahoo.fr>
2015-11-27 11:39:48 +0100

    added initial skeleton for class BST

--- BinarySearchTree/BST.h
+++ BinarySearchTree/BST.h
@@ -7,9 +7,53 @@
 // any purpose.
 //
 // BST.H
+//  This file declares a template class for a key-value data structure based on BST
 //
 // Author: Kevin Payet
 // ---------------------------------------------------------------------------
 
+#include <memory>
+
+template <class Key, class Item>
+class BST {
+    class Node;
+    typedef std::shared_ptr<Node> NodePtr;
+
+public:
+    BST():N(0) {}
+
+    /// Insert, retrieve, delete key-value pairs
+    void put(const Key &key, Item value);
+    Item get(const Key &key);
+    void remove(const Key &key);
+
+    /// Describe current state of the tree
+    bool contains(const Key &key);
+    int size();
+    bool isEmpty();
+
+    /// return all keys. Implement inorder DFS, BFS, ... ?
+
+
+private:
+    /// a BST is defined merely by a reference to the root Node
+    int N;  // number of nodes
+    NodePtr root;
+
+    class Node{
+        friend class BST<Key, Item>;
+
+        Key key;
+        Item value;
+        NodePtr left, right;
+
+        Node(const Key &key, Item value) {
+            this->key = key;
+            this->value = value;
+            // no need to initialize left and right to null, shared_ptr does it for us.
+        }
+    };
+
+};
 
 #endif // BST_H
